4cf305fd9554ec3e5fd47ace44ee5ddde9e665f3
nperez-d <nperezd42@gmail.com>
2025-02-07 22:09:22 +0100

    split render.c to adjust to norm

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/01/31 14:59:06 by nperez-d          #+#    #+#              #
-#    Updated: 2025/02/07 17:49:41 by nperez-d         ###   ########.fr        #
+#    Updated: 2025/02/07 22:07:49 by nperez-d         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,8 +21,8 @@ LIBFT_DIR	= libft/libft/
 PRINTF_DIR	= ft_printf/
 
 #Source files
-SRCS 	= $(SRC_DIR)main.c $(SRC_DIR)events.c $(SRC_DIR)map.c \
-			$(SRC_DIR)validate_map.c $(SRC_DIR)render.c \
+SRCS 	= $(SRC_DIR)main.c $(SRC_DIR)events.c $(SRC_DIR)init_game.c \
+			$(SRC_DIR)map.c $(SRC_DIR)validate_map.c $(SRC_DIR)render_map.c \
 			$(SRC_DIR)get_next_line.c $(SRC_DIR)get_next_line_utils.c
 OBJS 	= $(SRCS:$(SRC_DIR)%.c=$(OBJ_DIR)%.o)
 
---
+++ src/init_game.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_game.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 22:03:00 by nperez-d          #+#    #+#             */
+/*   Updated: 2025/02/07 22:03:53 by nperez-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static void	*load_image(void *mlx, char *path, int size)
+{
+	void	*img;
+
+	img = mlx_xpm_file_to_image(mlx, path, &size, &size);
+	if (!img)
+		ft_printf("Error: Failed to load texture %s\n", path);
+	return (img);
+}
+
+static int	load_all_images(t_game *game)
+{
+	game->img_wall = load_image(game->mlx, "textures/wall.xpm", game->img_size);
+	game->img_floor = load_image(game->mlx, "textures/floor.xpm", \
+		game->img_size);
+	game->img_player = load_image(game->mlx, "textures/player.xpm", \
+		game->img_size);
+	game->img_collectible = load_image(game->mlx, "textures/collectible.xpm", \
+		game->img_size);
+	game->img_exit = load_image(game->mlx, "textures/exit.xpm", game->img_size);
+	if (!game->img_wall || !game->img_floor || !game->img_player || \
+		!game->img_collectible || !game->img_exit)
+	{
+		free_game(game);
+		return (0);
+	}
+	return (1);
+}
+
+int	init_game(t_game *game)
+{
+	game->mlx = NULL;
+	game->win = NULL;
+	game->img_wall = NULL;
+	game->img_floor = NULL;
+	game->img_player = NULL;
+	game->img_collectible = NULL;
+	game->img_exit = NULL;
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		return (0);
+	game->img_size = 32;
+	game->win = mlx_new_window(game->mlx, game->map.width * game->img_size, \
+		game->map.height * game->img_size, "so_long");
+	if (!game->win)
+	{
+		free_game(game);
+		return (0);
+	}
+	if (!load_all_images(game))
+	{
+		free_game(game);
+		return (0);
+	}
+	return (1);
+}
