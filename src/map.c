631a8faf3cc72282d17608ecdf2aeb854aea5abb
nperez-d <nperezd42@gmail.com>
2025-02-07 10:19:18 +0100

    begun implementing map reader

--- src/map.c
+++ src/map.c
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/31 16:39:50 by nperez-d          #+#    #+#             */
-/*   Updated: 2025/02/07 10:05:18 by nperez-d         ###   ########.fr       */
+/*   Created: 2025/02/07 10:02:12 by nperez-d          #+#    #+#             */
+/*   Updated: 2025/02/07 10:02:13 by nperez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
-
-# include <mlx.h>
-# include <X11/X.h>
-# include <X11/keysym.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdio.h>
-
-int	handle_key(int key, void *mlx);
-int	handle_close(void *mlx);
-
-#endif
